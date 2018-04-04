7859b1806650b285d5a0cb0f97d9abc6442c6d61
Andrea <andrea.wayte@gmail.com>
2018-04-04 14:25:41 -0700

    user can only vote once

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,20 +1,36 @@
 class VotesController < ApplicationController
+  before_action :authenticate_user!
 
   def upvote
     vote = Vote.find_by(comment_id: params[:id], user_id: current_user.id)
     comment = Comment.find(params[:id])
     if !vote
-      comment.votes.create(comment_id: params[:id], user_id: current_user, vote_type: true)
-      comment = Comment.find(params[:id])
-    end 
+      comment.votes.create(comment_id: params[:id], user_id: current_user.id, vote_type: true)
+    else 
+      destroy vote 
+    end
+    comment = Comment.find(params[:id])
     render json: comment
   end 
 
   def downvote
+    vote = Vote.find_by(comment_id: params[:id], user_id: current_user.id)
     comment = Comment.find(params[:id])
-    comment.votes.create(comment_id: params[:id], user_id: current_user, vote_type: false)
+    if !vote
+      comment.votes.create(comment_id: params[:id], user_id: current_user.id, vote_type: false)
+    else 
+      destroy vote 
+    end 
     comment = Comment.find(params[:id])
     render json: comment
+  end
+
+  private
+
+  def destroy(vote)
+    if current_user.id == vote.user_id
+      vote.destroy
+    end 
   end
 
 end
