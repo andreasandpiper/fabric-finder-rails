1da4e11b6e794ca910229f8763322cc7884c3798
Andrea <andrea.wayte@gmail.com>
2018-04-04 13:10:28 -0700

    add routes for upvote and downvote

---
+++ app/controllers/votes_controller.rb
@@ -0,0 +1,9 @@
+class VotesController < ApplicationController
+
+  def upvote
+  end 
+
+  def downvote
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   resources :profile
   resources :comments
   get '/post/:id/comments', to: 'comments#get_comments'
+  post '/comments/:id/upvote', to: 'votes#upvote'
+  post '/comments/:id/downvote', to: 'votes#downvote'
 end
