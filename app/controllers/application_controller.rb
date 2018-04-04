6c8329c98d78ea453bfa67d416a57426ee0a46f0
Andrea <andrea.wayte@gmail.com>
2018-04-04 12:16:58 -0700

    redirect root to app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
 
+  def home 
+    redirect_to '/app'
+  end 
+
   protected
     def after_sign_in_path_for(resource)
       return '/app'
