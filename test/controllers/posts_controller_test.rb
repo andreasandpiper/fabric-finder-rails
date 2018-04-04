dd9b8873989d59ec3edb705794f1d82a79f49bff
Andrea <andrea.wayte@gmail.com>
2018-04-04 11:21:05 -0700

    basic post controller test

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -6,11 +6,20 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
     @post = posts(:one)
   end 
 
-  # test "should redirect create if not logged in" do 
-  #   assert_no_difference 'Post.count' do 
-  #     post posts_path, params: { post: { image: "image url", description: "description!"}}
-  #   end 
+  def json_response 
+    ActiveSupport::JSON.decode @response.body
+  end
 
-  #   assert_redirected_to user_session_url
-  # end 
+  test "should redirect create if not logged in" do 
+    assert_no_difference 'Post.count' do 
+      post posts_path, params: { post: { image: "image url", description: "description!"}}
+    end 
+  end 
+
+  test "should redirect destroy if not logged in" do 
+    assert_no_difference 'Post.count' do 
+      delete  post_path(@post)
+    end 
+  end
+
 end
