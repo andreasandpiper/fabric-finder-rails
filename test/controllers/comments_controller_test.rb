4e221a5a9b1cb517728a5e381c8606f7823c3e58
Andrea <andrea.wayte@gmail.com>
2018-04-04 11:33:17 -0700

    begin comments tests

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+    
+  def setup 
+    @post = posts(:one)
+    @comment = @post.comments.build(content: "hello")
+  end
+
+  test "should redirect create if not logged in" do 
+    assert_no_difference 'Comment.count' do 
+      post comments_path, params: { comment: {  content: "description!"}}
+    end 
+  end 
+
 end
--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,15 +1,11 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  
-  def setup
+
+  def setup 
     @post = posts(:one)
   end 
-
-  def json_response 
-    ActiveSupport::JSON.decode @response.body
-  end
-
+  
   test "should redirect create if not logged in" do 
     assert_no_difference 'Post.count' do 
       post posts_path, params: { post: { image: "image url", description: "description!"}}
