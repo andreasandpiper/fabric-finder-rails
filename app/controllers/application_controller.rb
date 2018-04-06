7ec2b74583562c2eb5d2db94a53156a32c50b9c6
Andrea <andrea.wayte@gmail.com>
2018-04-06 09:02:20 -0700

    add username to devise authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def home 
     redirect_to '/app'
@@ -13,5 +14,11 @@ class ApplicationController < ActionController::Base
     def after_sign_out_path_for(resource)
       return '/app'
     end
+
+    def configure_permitted_parameters
+      added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+      devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+      devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    end 
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ApplicationRecord
   validates :image,  presence: true
   validates :description, presence: true, length: { maximum: 1000 }
   attr_accessor :comment_count
-
+  
   def comment_count
     Post.find(self.id).comments.count
   end 
