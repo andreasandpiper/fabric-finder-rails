b3f65ec7f855a62fdcb3feef1e2b439573885f04
Andrea <andrea.wayte@gmail.com>
2018-04-20 12:37:30 -0700

    add mailer config for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,6 +99,20 @@ Rails.application.configure do
       }
     }
 
+    config.action_mailer.default_url_options = { :host => 'https://young-ocean-52714.herokuapp.com' }
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      user_name:      ENV['SENDMAIL_USERNAME'],
+      password:       ENV['SENDMAIL_PASSWORD'],
+      domain:         ENV['MAIL_HOST'],
+      address:       'smtp.gmail.com',
+      port:          '587',
+      authentication: :plain,
+      enable_starttls_auto: true
+    }
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
