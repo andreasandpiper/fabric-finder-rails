c947b7a9134837bc513bd938dfb3c7a25e03d77f
Andrea <andrea.wayte@gmail.com>
2018-04-30 20:48:18 -0700

    remove cat quote in footer

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -5,7 +5,6 @@ export default () => {
     <footer className="footer">
       <div className="container">
         <div className="content has-text-centered">
-          <p>If my fabric collection were cats, I would be swimming in a sea of cats.</p>
           <p>
             <strong>&copy; <a href="https://www.andreawayte.com" target="_blank">Andrea Wayte</a> | 2018</strong>
           </p>
